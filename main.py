f34eab795b23dc865c2efa3c8bd0d9a8003732fe
damir-ilyasov <damir2006ilyasov@gmail.com>
2026-03-11 17:33:01 +0300

    3.2

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr, field_validator
+from typing import Optional
 
 app = FastAPI()
-class UserCreate(BaseModel):
-    name: str
-    email: EmailStr
-    age: int
-    is_subscribed: bool
-    
-    @field_validator("age")
-    @classmethod
-    def age_validate(cls, v):
-        if v is not None and v <= 0:
-            raise ValueError("Возраст не мможет быть меньше 0")
-        return v
+products = [
+    {"product_id": 123, "name": "Smartphone",  "category": "Electronics", "price": 599.99},
+    {"product_id": 456, "name": "Phone Case",  "category": "Accessories", "price": 19.99},
+    {"product_id": 789, "name": "Iphone",       "category": "Electronics", "price": 1299.99},
+    {"product_id": 101, "name": "Headphones",  "category": "Accessories", "price": 99.99},
+    {"product_id": 202, "name": "Smartwatch",  "category": "Electronics", "price": 299.99},
+]
 
-@app.post('/create_users')
-async def create_users(user: UserCreate):
-    return user
+@app.get('/product/{product_id}')
+async def get_product_by_id(product_id: int):
+    for product in products:
+        if product["product_id"] == product_id:
+            return product
+
+@app.get("/products/search")
+def search_products(
+    keyword: str,
+    category: Optional[str] = None,
+    limit: int = 10
+    ):
+    results = []
+    for product in products:
+        if keyword.lower() not in product["name"].lower():
+            continue
+        if category and product["category"].lower() != category.lower():
+            continue
+        results.append(product)
+
+    return results[:limit]
