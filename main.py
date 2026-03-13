f1d6fc2518f7478f938a7019575b0642dda08bec
damir-ilyasov <damir2006ilyasov@gmail.com>
2026-03-13 20:37:06 +0300

    5.5

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-from fastapi import FastAPI, Header, HTTPException
-from typing import Optional
+from fastapi import FastAPI, Response
+from pydantic import BaseModel, field_validator
+from typing import Annotated
+from fastapi.params import Header
 import re
+import datetime
 app = FastAPI()
 
-def validate_accept_language(value: str) -> bool:
-    pattern = r'^([a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*(;q=0(\.\d{1,3})?)?)(,\s*[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*(;q=0(\.\d{1,3})?)?)*$'
-    return bool(re.match(pattern, value))
+class CommonHeaders(BaseModel):
+    user_agent: str = Header(alias="user-agent")
+    accept_language: str = Header(alias="accept-language")
+
+    model_config = {"populate_by_name": True}
+
+    @field_validator("accept_language")
+    @classmethod
+    def validate_accept_language(cls, v):
+        pattern = r'^([a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*(;q=0(\.\d{1,3})?)?)(,\s*[a-zA-Z]{1,8}(-[a-zA-Z0-9]{1,8})*(;q=0(\.\d{1,3})?)?)*$'
+        if not re.match(pattern, v):
+            raise ValueError("Неверный формат заголовка Accept-Language")
+        return v
 
 @app.get("/headers")
-def get_headers(
-    user_agent: Optional[str] = Header(default=None),
-    accept_language: Optional[str] = Header(default=None),
-):
-    if not user_agent:
-        raise HTTPException(status_code=400, detail="Заголовок User-Agent отсутствует")
+def get_headers(headers: Annotated[CommonHeaders, Header()]):
+    return {
+        "User-Agent": headers.user_agent,
+        "Accept-Language": headers.accept_language,
+    }
 
-    if not accept_language:
-        raise HTTPException(status_code=400, detail="Заголовок Accept-Language отсутствует")
-
-    if not validate_accept_language(accept_language):
-        raise HTTPException(status_code=400, detail="Неверный формат заголовка Accept-Language")
+@app.get("/info")
+def get_info(headers: Annotated[CommonHeaders, Header()], response: Response):
+    response.headers["X-Server-Time"] = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
     return {
-        "User-Agent": user_agent,
-        "Accept-Language": accept_language,
+        "message": "Добро пожаловать! Ваши заголовки успешно обработаны.",
+        "headers": {
+            "User-Agent": headers.user_agent,
+            "Accept-Language": headers.accept_language,
+        }
     }
